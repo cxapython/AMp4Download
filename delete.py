5ab3129174f8343c26b57031b3f6070aa652559d
Chen Xiang‘An <49929318+cxapython@users.noreply.github.com>
2023-03-10 15:22:29 +0800

    Update delete.py

--- delete.py
+++ delete.py
@@ -1,13 +1,10 @@
 import os
+import shutil
 
 def delete_mp4(folder_path):
-    files = os.listdir(folder_path)
-    originFile = folder_path.split(os.path.sep)[-1] + '.mp4'
-    for file in files:
-        if file != originFile and ".jpg" not in file:
-            os.remove(os.path.join(folder_path, file))
-
-
+    print("开始删除ts文件")
+    shutil.rmtree(folder_path)
+    print (f"删除成功:{folder_path}")
 def delete_m3u8(folder_path):
     files = os.listdir(folder_path)
     for file in files:
